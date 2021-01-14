60521419b3794ea6d69931315bf204851e7e28e3
misterhaywood <mr.haywood@gmail.com>
2021-01-14 08:53:10 -0600

    Update oojs.js

--- oojs.js
+++ oojs.js
@@ -20,7 +20,7 @@ class Playlist {
 // #2: Create an instance of the Playlist class and set it to a variable called `myPlaylist`
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
-let myPlaylist = new Playlist;
+var myPlaylist = new Playlist;
 myPlaylist.addSong("Shimmy Shimmy Ya")
 
 
