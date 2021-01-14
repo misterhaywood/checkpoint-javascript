00162b3a3f01dce75a9da56c5fd75d349aaca683
misterhaywood <mr.haywood@gmail.com>
2021-01-14 09:02:30 -0600

    Update fundamentals.js

--- fundamentals.js
+++ fundamentals.js
@@ -30,4 +30,5 @@ var instructor = {firstName:"Pete", lastName:"Woods", age:33};
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
-instructor.has-office-hours;
+instructor.has-office-hours = true;
+
